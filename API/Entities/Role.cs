fb4925cfeb80c49f133c37f284c16291876265bd
Lukas <likkamattiziukas@gmail.com>
2019-03-18 11:14:43 +0200

    pataisymas

--- API/Entities/Role.cs
+++ API/Entities/Role.cs
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-﻿
-namespace WebApi.Entities
-{
-    public class Role
-=======
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +7,6 @@ using System.Threading.Tasks;
 namespace WebApi.Entities
 {
     public static class Role
->>>>>>> 050215db5888b4bd489eacf3e31ad7d19694412d
     {
         public const string Admin = "Admin";
         public const string User = "User";
--- API/Entities/Room.cs
+++ API/Entities/Room.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.ComponentModel.DataAnnotations.Schema;
-=======
->>>>>>> 050215db5888b4bd489eacf3e31ad7d19694412d
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -12,20 +9,11 @@ namespace WebApi.Entities
 {
     public class Room 
     {
-<<<<<<< HEAD
         public int roomId { get; set; }
         public int roomAdminId { get; set; }
         public string roomName { get; set; }
         [NotMapped]
         public int[] users { get; set; }
-=======
-      
-
-        public int roomId { get; set; }
-        public int roomAdminId { get; set; }
-        public string roomName { get; set; }
-        
->>>>>>> 050215db5888b4bd489eacf3e31ad7d19694412d
     }
 }
        
