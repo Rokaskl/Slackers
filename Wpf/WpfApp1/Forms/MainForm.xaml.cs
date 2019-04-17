e57150bf9cba072df51486cb7054c8a5dc61340f
Jokūbas Jurčiukonis <jokjur1@ktu.lt>
2019-04-17 13:34:47 +0300

    WPF ablity to create room and display user as admin rooms list

--- Wpf/WpfApp1/Forms/MainForm.xaml.cs
+++ Wpf/WpfApp1/Forms/MainForm.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,7 +14,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using Json.Net;
+using WebApi.Dtos;
+using Newtonsoft.Json;
+using WpfApp1.Forms;
 
 namespace WpfApp1.Forms
 {
@@ -19,11 +28,22 @@ namespace WpfApp1.Forms
     /// </summary>
     public partial class MainForm : Window
     {
+        private HttpClient client;
         public MainForm()
         {
+            Inst.CreateInstance();
+            client = Inst.Utils.HttpClient;
+            LoginForm loginForm = new LoginForm();
+            if (!(loginForm.ShowDialog() ?? false))
+            {
+                //Application.Exit
+                //Environment.Exit
+                this.Close();
+                return;
+            }
             InitializeComponent();
+            frame.NavigationService.Navigate(new RoomsPage());
         }
-
         private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
         {
             ButtonOpenMenu.Visibility = Visibility.Collapsed;
---
+++ Wpf/WpfApp1/Forms/RoomsPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using Json.Net;
+using WebApi.Dtos;
+using Newtonsoft.Json;
+using WpfApp1.Forms;
+using System.Globalization;
+
+namespace WpfApp1.Forms
+{
+    /// <summary>
+    /// Interaction logic for RoomsPage.xaml
+    /// </summary>
+    public partial class RoomsPage : Page
+    {
+        private HttpClient client;
+        public RoomsPage()
+        {
+            client = Inst.Utils.HttpClient;
+            ShowRooms();
+            InitializeComponent();
+        }
+
+        private void BtnRegisterRoom_Click(object sender, RoutedEventArgs e)
+        {
+            if (roomNameText.Text != "")
+                RegisterRoom(roomNameText.Text);
+            else
+                MessageBox.Show("Theres no room name");
+        }
+
+        private async void ShowRooms()
+        {
+            try
+            {
+                var res = await client.GetAsync("Rooms/admin_get_rooms");
+                List<Dictionary<string, string>> adminR = res.Content.ReadAsAsync<List<Dictionary<string, string>>>().Result;
+                adminRooms.ItemsSource = adminR;
+
+                /*var res2 = await client.GetAsync("Rooms/user_get_rooms");
+                List<Dictionary<string, string>> userR = res2.Content.ReadAsAsync<List<Dictionary<string, string>>>().Result;
+                userRooms.ItemsSource = userR;*/
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private async void RegisterRoom(String name)
+        {
+            try
+            {
+                var club = new RoomDto()
+                {
+                    roomName = name
+                };
+                var response = await client.PostAsJsonAsync("/Rooms/register", club);
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Register Successfully");
+                    ShowRooms();
+                }
+                else
+                {
+                    MessageBox.Show("Register Failed...");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
