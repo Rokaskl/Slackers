78b2836ff7dd3e6754a69d725c416db8d6c922fe
Jokūbas Jurčiukonis <jokjur1@ktu.lt>
2019-05-28 19:37:07 +0300

    User ability to change photo

--- Wpf/WpfApp1/Pages/AccountPage.xaml.cs
+++ Wpf/WpfApp1/Pages/AccountPage.xaml.cs
@@ -1,24 +1,22 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Net.Http;
 using WebApi.Dtos;
+using WebApi.Entities;
 using WpfApp1.Forms;
-using System.Diagnostics;
-using WpfApp1.TimerControl;
-using System.Reflection;
 
 namespace WpfApp1.Pages
 {
@@ -27,13 +25,15 @@ namespace WpfApp1.Pages
     /// </summary>
     public partial class AccountPage : Page
     {
+        private byte[] photo = null;
         public AccountPage()
         {
             InitializeComponent();
-            showInfo();
+            ShowInfo();
+            SetProfilePhoto();
         }
 
-        private void showInfo()
+        private void ShowInfo()
         {
             txtUsername.Text = Inst.ApiRequests.User.username;
             txtName.Text = Inst.ApiRequests.User.firstName;
@@ -44,6 +44,55 @@ namespace WpfApp1.Pages
         {
             PassChangingForm passform = new PassChangingForm();
             passform.Show();
+        }
+
+        private void BtnChangePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".jpg";
+            dialog.Filter = "Text documents (.jpg)|*.jpg";
+            if (dialog.ShowDialog().HasValue && File.Exists(dialog.FileName))
+            {
+                photo = File.ReadAllBytes(dialog.FileName);
+                ChangeData();
+            }
+        }
+
+        private async void ChangeData()
+        {
+            int userId = Int32.Parse(Inst.ApiRequests.User.id);
+            AdditionalData data = new AdditionalData(userId, true, "", photo);
+            if (await Inst.ApiRequests.UpdateAddDataUser(data))
+            {
+                MessageBox.Show("Photo upload succsesful");
+            }
+            else
+            {
+                MessageBox.Show("Photo upload failed");
+            }
+            SetProfilePhoto();
+        }
+
+        private async void SetProfilePhoto()
+        {
+            if (await Inst.ApiRequests.UserLoginAddData())
+            {
+                if (Inst.ApiRequests.AdditionalData != null)
+                {
+                    if (Inst.ApiRequests.AdditionalData.PhotoBytes != null)
+                    {
+                        using (var memstr = new MemoryStream(Inst.ApiRequests.AdditionalData.PhotoBytes))
+                        {
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.CacheOption = BitmapCacheOption.OnLoad;
+                            image.StreamSource = memstr;
+                            image.EndInit();
+                            profilePicture.ImageSource = image;
+                        }
+                    }
+                }
+            }
         }
     }
 }
